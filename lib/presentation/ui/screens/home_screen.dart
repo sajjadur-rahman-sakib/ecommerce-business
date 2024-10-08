d86b7ca4419626843ca2db39ea2d6cd3912d4e69
Sajjadur Rahman Sakib <sajjadur.rahman.sakib.x@gmail.com>
2024-10-08 16:55:15 +0600

    Commit

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -1,7 +1,9 @@
+import 'package:ecommerce/presentation/ui/utils/app_colors.dart';
 import 'package:ecommerce/presentation/ui/utils/assets_path.dart';
 import 'package:ecommerce/presentation/ui/widgets/app_bar_icon_button.dart';
 import 'package:ecommerce/presentation/ui/widgets/home_banner_slider.dart';
 import 'package:ecommerce/presentation/ui/widgets/search_text_field.dart';
+import 'package:ecommerce/presentation/ui/widgets/section_header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -28,8 +30,60 @@ class _HomeScreenState extends State<HomeScreen> {
             const SizedBox(height: 16),
             const HomeBannerSlider(),
             const SizedBox(height: 16),
+            _buildCategoriesSection(),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildCategoriesSection() {
+    return Column(
+      children: [
+        SectionHeader(
+          title: 'Categories',
+          onTap: () {},
+        ),
+        const SizedBox(height: 8),
+        SizedBox(
+          height: 140,
+          child: _buildCategoryListView(),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCategoryListView() {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: AppColors.themeColor.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: const Icon(
+                Icons.computer,
+                size: 48,
+                color: AppColors.themeColor,
+              ),
+            ),
+            const SizedBox(height: 4),
+            const Text(
+              'Electronics',
+              style: TextStyle(
+                color: AppColors.themeColor,
+              ),
+            )
+          ],
+        );
+      },
+      separatorBuilder: (_, __) => const SizedBox(
+        width: 8,
       ),
     );
   }
